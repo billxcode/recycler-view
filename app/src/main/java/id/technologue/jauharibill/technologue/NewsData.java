e2e0806e046546d313021c9102ba8ba3bb5b86b5
bill tanthowi jauhari <xcodebill@gmail.com>
2018-07-02 17:30:41 +0700

    add detail news

--- app/src/main/java/id/technologue/jauharibill/technologue/NewsData.java
+++ app/src/main/java/id/technologue/jauharibill/technologue/NewsData.java
@@ -12,11 +12,11 @@ public class NewsData {
 
     public static String[][] dataNews = new String[][]{
             {"Setelah Fortnite, Ini Game yang Terancam Gagal Cross-Play Karena Sony", "Ulwan Fakhri Noviadhista", "July 2, 2018", "https://technologue.id/wp-content/uploads/2018/07/Fallout-76-source-YouTube-Bethesda-Softworks-324x160.jpg"},
-            {"Setelah Fortnite, Ini Game yang Terancam Gagal Cross-Play Karena Sony", "Ulwan Fakhri Noviadhista", "July 2, 2018", "https://technologue.id/wp-content/uploads/2018/07/Fallout-76-source-YouTube-Bethesda-Softworks-324x160.jpg"},
-            {"Setelah Fortnite, Ini Game yang Terancam Gagal Cross-Play Karena Sony", "Ulwan Fakhri Noviadhista", "July 2, 2018", "https://technologue.id/wp-content/uploads/2018/07/Fallout-76-source-YouTube-Bethesda-Softworks-324x160.jpg"},
-            {"Setelah Fortnite, Ini Game yang Terancam Gagal Cross-Play Karena Sony", "Ulwan Fakhri Noviadhista", "July 2, 2018", "https://technologue.id/wp-content/uploads/2018/07/Fallout-76-source-YouTube-Bethesda-Softworks-324x160.jpg"},
-            {"Setelah Fortnite, Ini Game yang Terancam Gagal Cross-Play Karena Sony", "Ulwan Fakhri Noviadhista", "July 2, 2018", "https://technologue.id/wp-content/uploads/2018/07/Fallout-76-source-YouTube-Bethesda-Softworks-324x160.jpg"},
-            {"Setelah Fortnite, Ini Game yang Terancam Gagal Cross-Play Karena Sony", "Ulwan Fakhri Noviadhista", "July 2, 2018", "https://technologue.id/wp-content/uploads/2018/07/Fallout-76-source-YouTube-Bethesda-Softworks-324x160.jpg"},
+            {"Facebook Incar Hak Siar Liga Inggris", "Ulwan Fakhri Noviadhista", "July 2, 2018", "https://technologue.id/wp-content/uploads/2018/07/Ilustrasi-Facebook-source-Facebook--324x160.jpg"},
+            {"Agustus 2018, Motorola Punya Smartphone Flagship Baru?", "Aqmal Maulana", "July 2, 2018", "https://technologue.id/wp-content/uploads/2018/07/motorola-2018-event-324x160.jpg"},
+            {"Grab-Bosowa Kolaborasi, Siap Bahu-membahu di Aspal", "Ulwan Fakhri Noviadhista", "July 1, 2018", "https://technologue.id/wp-content/uploads/2018/07/Ongki-Kurniawan-Executive-Director-Grab-Indonesia-kanan-berbincang-bersama-Subhan-Aksa-CEO-Bosowa-Automotive-Group-di-dep-324x160.jpg"},
+            {"Setelah Sedekade, Akhirnya Teknologi Wi-Fi Terima Security Update", "Choiru Rizkia", "July 1, 2018", "https://technologue.id/wp-content/uploads/2018/06/wifi-324x160.jpg"},
+            {"Flash Sale Infinix Hot 6 Pro Ditunda Tanggal Segini", "Ulwan Fakhri Noviadhista", "July 1, 2018", "https://technologue.id/wp-content/uploads/2018/07/Peresmian-Infinix-Hot-6-Pro-di-Indonesia-eksklusif-Technologue.id_-324x160.jpg"},
     };
 
     public static ArrayList<News> getListData(){
